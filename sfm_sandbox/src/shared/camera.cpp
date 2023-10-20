09173549af7500e14608ba085c4f3ed9fd762689
Jack Scott <jack.scott6@hotmail.co.uk>
2023-10-21 01:16:14 +1100

    added default values for camera constructor, added clangformat

--- sfm_sandbox/src/shared/camera.cpp
+++ sfm_sandbox/src/shared/camera.cpp
@@ -1,31 +1,40 @@
 #include "camera.h"
 
-Camera::Camera(std::string name, CameraModel model, DistortionModel distortion_model, cv::Mat K, cv::Mat distortion, cv::Size resolution) : name(name), model(model), distortion_model(distortion_model), K(K), distortion(distortion), resolution(resolution) {
+Camera::Camera(std::string name, CameraModel model, DistortionModel distortion_model,
+               cv::Mat K = cv::Mat::eye(3, 3, CV_64F),
+               cv::Mat distortion = cv::Mat::zeros(1, 5, CV_64F), cv::Size resolution = cv::Size())
+    : name(name),
+      model(model),
+      distortion_model(distortion_model),
+      K(K),
+      distortion(distortion),
+      resolution(resolution) {
+  // constructor body
 }
 
 void Camera::undistortImage(const cv::Mat& image, cv::Mat& undistorted_image) {
-    if (distortion_model == DistortionModel::NONE) {
-        undistorted_image = image.clone();
-        return;
-    }else if (distortion_model == DistortionModel::RADTAN) {
-        cv::undistort(image, undistorted_image, K, distortion);
-        return;
-    }else if (distortion_model == DistortionModel::EQUIDISTANT) {
-        cv::fisheye::undistortImage(image, undistorted_image, K, distortion);
-        return;
-    }
-    
+  if (distortion_model == DistortionModel::NONE) {
+    undistorted_image = image.clone();
+    return;
+  } else if (distortion_model == DistortionModel::RADTAN) {
+    cv::undistort(image, undistorted_image, K, distortion);
+    return;
+  } else if (distortion_model == DistortionModel::EQUIDISTANT) {
+    cv::fisheye::undistortImage(image, undistorted_image, K, distortion);
+    return;
+  }
 }
 
-void Camera::undistortPoints(const std::vector<cv::Point2f>& points, std::vector<cv::Point2f>& undistorted_points) {
-    if (distortion_model == DistortionModel::NONE) {
-        undistorted_points = points;
-        return;
-    }else if (distortion_model == DistortionModel::RADTAN) {
-        cv::undistortPoints(points, undistorted_points, K, distortion);
-        return;
-    }else if (distortion_model == DistortionModel::EQUIDISTANT) {
-        cv::fisheye::undistortPoints(points, undistorted_points, K, distortion);
-        return;
-    }
+void Camera::undistortPoints(const std::vector<cv::Point2f>& points,
+                             std::vector<cv::Point2f>& undistorted_points) {
+  if (distortion_model == DistortionModel::NONE) {
+    undistorted_points = points;
+    return;
+  } else if (distortion_model == DistortionModel::RADTAN) {
+    cv::undistortPoints(points, undistorted_points, K, distortion);
+    return;
+  } else if (distortion_model == DistortionModel::EQUIDISTANT) {
+    cv::fisheye::undistortPoints(points, undistorted_points, K, distortion);
+    return;
+  }
 }
