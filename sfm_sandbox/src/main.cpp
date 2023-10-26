e56762c33c03ff540c88c87e6f660f6f5f866998
Jack Scott <jack.scott6@hotmail.co.uk>
2023-10-27 02:06:22 +1100

    main function can display images with keypoints overlaid

--- sfm_sandbox/src/main.cpp
+++ sfm_sandbox/src/main.cpp
@@ -7,6 +7,9 @@
 #include "shared/frame.h"
 #include "shared/image.h"
 
+#include <opencv2/opencv.hpp>
+#include "open3d/Open3D.h"
+
 using namespace sfm;
 
 int main(int argc, char** argv) {
@@ -32,7 +35,24 @@ int main(int argc, char** argv) {
     std::cout << frame->getImage()->getName() << std::endl;
     std::cout << frame->getDescriptors().size() << std::endl;
     std::cout << frame->getKeypoints().size() << std::endl;
+    cv::Mat image_with_points;
+    cv::drawKeypoints(*frame->getImage()->getData(), frame->getKeypoints(), image_with_points);
+    cv::imshow("Keypoints", image_with_points);
+    cv::waitKey(0);
   }
+
+  for (auto point : frame_bundle[0]->getKeypoints()) {
+    std::cout << point.pt << std::endl;
+  }
+
+  // Display cv mat in open3d
+  // cv::Mat cv_frame = *frame_bundle[0]->getImage()->getData();
+  // std::shared_ptr im_ptr = std::make_shared<open3d::geometry::Image>();
+  // // Allocate data buffer
+  // im_ptr->Prepare(cv_frame.size[0], cv_frame.size[1], 3, 1);
+  // // mat is your cv::Mat depth image
+  // memcpy(im_ptr->data_.data(), cv_frame.data, im_ptr->data_.size());
+  // open3d::visualization::DrawGeometries({im_ptr}, "Image", im_ptr->width_, im_ptr->height_);
 
   return 0;
 }
